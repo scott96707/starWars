8231149129587dbfc8954ad36a3b896987c849bc
scott96707 <scott96707@gmail.com>
2019-01-11 04:36:36 -0700

    Setup to be a copy of the robots project to use as a template

--- src/components/Cards.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Card = ({ name, gravity, })
-
-export default Card;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox';
+import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            planets = [],
-            searchfield = '',
+            robots: [],
+            //planets: [],
+            searchfield: '',
         }
     };
 
-    render() {
-        const { planets, searchfield } = this.state;
-        const filteredPlanets = planets.filter(planet => {
-            return planet.name
-        })
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
 
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className="tc">
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    }
 }
 
 export default App;
