a031df4c4cb8602819a3c4de0bc19690650da25f
scott96707 <scott96707@gmail.com>
2019-05-02 15:38:47 -0600

    Added Alderaan image, converted Cards to a const from a class

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,36 +1,25 @@
 import React, { Component } from "react";
-import yavin from './yavin.jpg';
 import './Card.css';
 
-class Card extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cardSide: true,
-        };
-    };
-    
-    render() {
-        const { name, climate, rotation_period, orbital_period, diameter } = this.props;
-
-        return (
-            <div className="flip-card" id={name}>
-                <div className='flip-card-inner dib bg-light-blue br3 pa3 ma4 shadow-6' style={{width: 235, height: 303}}>
-                    <div className="flip-card-front br3">
-                        <img alt='planet' src={yavin} width="200" height="200"/>
-                            <h2>{name}</h2>
-                    </div>
-                    <div className="flip-card-back br3">
+const Card = ({ name, image, climate, rotation_period, orbital_period, diameter }) => {
+    return (
+        <div className="flip-card" id={name}>
+            <div className='flip-card-inner dib bg-light-blue br3 pa3 ma4 shadow-6' style={{width: 235, height: 303}}>
+                <div className="flip-card-front br3">
+                    <img alt='planet' src={image} width="200" height="200"/>
                         <h2>{name}</h2>
-                        <p>Climate: {climate}</p>
-                        <p>Day Length: {rotation_period}</p>
-                        <p>Year Length: {orbital_period}</p>
-                        <p>Planetary Diameter: {diameter}</p>
-                    </div> 
                 </div>
+                <div className="flip-card-back br3">
+                    <h2>{name}</h2>
+                    <p>Climate: {climate}</p>
+                    <p>Day Length: {rotation_period}</p>
+                    <p>Year Length: {orbital_period}</p>
+                    <p>Planetary Diameter: {diameter}</p>
+                </div> 
             </div>
-        );
-    }
+        </div>
+    );
 }
+
 
 export default Card;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,8 @@ import React from "react";
 import Card from './Card';
 import './Cardlist.css';
 
+import alderaan from '../images/alderaan.jpg';
+
 const CardList = ({ planets }) => {
     const cardComponent = planets.map((entry, i) => {
         return (
@@ -9,6 +11,7 @@ const CardList = ({ planets }) => {
                 key={i}
                 orbital_period={planets[i].orbital_period}
                 name={planets[i].name}
+                image={alderaan}
                 climate={planets[i].climate}
                 rotation_period={planets[i].rotation_period}
                 diameter={planets[i].diameter}
