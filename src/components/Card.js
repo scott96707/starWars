f49a3e660afed75dd58c316a0e8c7b4b231f6d6d
scott96707 <scott96707@gmail.com>
2019-05-02 14:44:03 -0600

    Fixed animation, changed style of cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,38 +11,22 @@ class Card extends Component {
     };
     
     render() {
-
-        const handleToggleClick = () => {
-            this.setState(prevState => ({
-                cardSide: !prevState.cardSide}));
-            let el = document.getElementById(name);
-            el.classList.toggle("flipper");
-        };
-
         const { name, climate, rotation_period, orbital_period, diameter } = this.props;
-        const { cardSide } = this.state;
 
-        const CardSide = () => {
-            return (cardSide) ? (
-                    <div className="cardFront">
-                    <img alt='planet' src={yavin} width="200" height="200"/>
-                        <h2>{name}</h2>
+        return (
+            <div className="flip-card" id={name}>
+                <div className='flip-card-inner dib bg-light-blue br3 pa3 ma4 shadow-6' style={{width: 235, height: 303}}>
+                    <div className="flip-card-front br3">
+                        <img alt='planet' src={yavin} width="200" height="200"/>
+                            <h2>{name}</h2>
                     </div>
-            ) : (
-                    <div className="cardBack">
+                    <div className="flip-card-back br3">
                         <h2>{name}</h2>
                         <p>Climate: {climate}</p>
                         <p>Day Length: {rotation_period}</p>
                         <p>Year Length: {orbital_period}</p>
                         <p>Planetary Diameter: {diameter}</p>
                     </div> 
-            )
-        };
-
-        return (
-            <div className="cards" id={name}>
-                <div onClick={handleToggleClick}  className='dib bg-light-blue br3 pa3 ma4 bw2 shadow-5' style={{width: 235, height: 303}}>
-                    <CardSide side={cardSide} />
                 </div>
             </div>
         );
