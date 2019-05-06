e2bf94407b675939107a6d9fa578ef76f2796253
scott96707 <scott96707@gmail.com>
2019-05-06 14:43:02 -0600

    Changed cards to flip on active, not hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import './Card.css';
 
 const Card = ({ name, image, climate, rotation_period, orbital_period, diameter }) => {
