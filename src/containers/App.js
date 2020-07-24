e0897032a0d9ba2f3f7115901fee46e9b35828de
scott96707 <scott96707@gmail.com>
2020-07-24 07:21:14 -0600

    Removed React Particles for now to get Prod up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,7 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Particles from 'react-particles-js';
 import './App.css';
-
-const particlesOptions ={
-    "particles": {
-        "number": {
-            "value": 60,
-            "density": {
-                "enable": true,
-                "value_area": 1000
-            }
-        },
-        "line_linked": {
-            "enable": false
-        },
-        "move": {
-            "speed": 0
-        },
-        "size": {
-            "value": 1.75
-        },
-        "opacity": {
-            "anim": {
-                "enable": true,
-                "speed": 1,
-                "opacity_min": 0.05
-            }
-        },
-    },
-    "interactivity": {
-        "events": {
-            "onclick": {
-                "enable": true,
-                "mode": "push"
-            }
-        },
-        "modes": {
-            "push": {
-                "particles_nb": 1
-            }
-        }
-    },
-}
 
 class App extends Component {
     constructor() {
@@ -73,7 +31,6 @@ class App extends Component {
         return !planets.length ? <h1>Loading Planets</h1> :
             (
                 <div className="tc">
-                <Particles className="particles" params={particlesOptions} />
                     <h1 className='f1'> Star Tours </h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     {/* <div style={{ display: "flex", justifyContent: "center" }}>
